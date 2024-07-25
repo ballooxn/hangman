04615b145f0315c88de4d41dbdb556c93f52c9fa
Lincoln <163782652+ballooxn@users.noreply.github.com>
2024-07-25 17:59:29 -0500

    Finish base game

--- lib/display.rb
+++ lib/display.rb
@@ -6,4 +6,29 @@ module Display
     puts "You will have 6 attempts to guess the word."
     puts "Good luck!"
   end
+
+  def self.display_number_of_letters(guessed_word)
+    puts guessed_word.join("")
+  end
+
+  def self.choose_letter
+    puts "Choose a letter:"
+  end
+
+  def self.display_feedback(guessed_word)
+    puts "Current progress: #{guessed_word}"
+  end
+
+  def self.incorrect_guess(guesses_remaining)
+    puts "Incorrect guess. You have #{guesses_remaining} guesses remaining."
+  end
+
+  def self.congrats_message(rounds_won)
+    rounds_won == 1 ? (puts "Congratulations! You won your first round!") : (puts "Congratulations! You won #{rounds_won} rounds.")
+  end
+
+  def self.game_over_message(rounds_won)
+    (puts "Game over! You won #{rounds_won} rounds.")
+    puts "Would you like to play again? (y/n)"
+  end
 end
--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -6,22 +6,23 @@ class GameController
   include Display
   @@rounds_won = 0
   def initialize
-    @selected_word = ""
-    @guess = ""
+    @selected_word = []
+    @guessed_word = []
     @file = "dictionary.txt"
     start_game
   end
 
-  def choose_random_line(line)
-    result = nil
-
-    File.open(@file, "r") do |f|
-      while line.positive?
-        line -= 1
-        result = f.gets
+  def choose_random_line
+    result = ["oh yeah"]
+    until result.length >= 5 && result.length <= 12
+      line = rand(1..10_000)
+      File.open(@file, "r") do |f|
+        line.times do
+          result = f.gets.chars
+          result.pop
+        end
       end
     end
-
     result
   end
 
@@ -31,14 +32,50 @@ class GameController
 
   def game_loop
     game_over = false
+    guesses_remaining = 6
     until game_over
+      p @selected_word = choose_random_line
+      @guessed_word = Array.new(@selected_word.length, "-")
+      Display.display_number_of_letters(@guessed_word)
+      until selected_word_guessed?
+        Display.choose_letter
+        guess_letter = gets.chomp.downcase
+        correct = make_feedback(guess_letter)
+        if correct
+          Display.display_feedback(@guessed_word)
+        else
+          guesses_remaining -= 1
+          Display.incorrect_guess(guesses_remaining) unless guesses_remaining.zero?
+        end
 
+        next unless guesses_remaining.zero?
+
+        game_over = true
+        break
+
+      end
+      # Word has been guessed
+      @@rounds_won += 1
+      Display.congrats_message(@@rounds_won)
     end
+    # The game is over
+    Display.game_over_message(@@rounds_won)
+    play_again = gets.chomp.downcase
+    GameController.new if play_again == "y"
   end
 
-  def guess_letter
+  def make_feedback(guess_letter)
+    letter_correct = false
+    @selected_word.each_with_index do |letter, i|
+      if letter == guess_letter
+        @guessed_word[i] = guess_letter
+        letter_correct = true
+      end
+    end
+    letter_correct
   end
 
-  def choose_word
+  def selected_word_guessed?
+    @guessed_word == @selected_word
   end
 end
